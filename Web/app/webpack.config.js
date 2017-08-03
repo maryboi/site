aec5cf3954ec0bcea2be6f8052d8a4220e3f1057
mathiasnohall <mathias.nohall@gmail.com>
2017-08-03 13:25:36 +0200

    added react and es2015

--- Web/app/webpack.config.js
+++ Web/app/webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         }]
+     }
 };
